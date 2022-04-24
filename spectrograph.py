11bf29938847e871943e8cde7136f8f0676f9fad
Daniel Harper <dghjunior@gmail.com>
2022-04-24 19:49:03 -0400

    Added reference guides

--- spectrograph.py
+++ spectrograph.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import os
 from scipy.io import wavfile
 
+# Source for spectrograph generation https://pythontic.com/visualization/signals/spectrogram
 
 def createSpectrogram(filename=''):
     if filename != '':
@@ -16,15 +17,21 @@ def createSpectrogram(filename=''):
         plt.xlabel('Time')
         plt.ylabel('Frequency')
 
-        #plt.show()
+        # plt.show()
         imgFilename = 'imgFiles/' + filename[9:len(filename)-3] + 'png'
         print(imgFilename)
         fig.savefig(imgFilename)
 
 directory = 'wavFiles'
-#os.remove('imgFiles')
-#os.mkdir('imgFiles')
+# os.remove('imgFiles')
+# os.mkdir('imgFiles')
 for file in os.listdir(directory):
     f = os.path.join(directory, file)
     if os.path.isfile(f):
         createSpectrogram(f)
+
+# Model building guides from Austin:
+
+# https://analyticsindiamag.com/guide-to-feed-forward-network-using-pytorch-with-mnist-dataset/
+# https://www.deeplearningwizard.com/deep_learning/practical_pytorch/pytorch_feedforward_neuralnetwork/
+# https://towardsdatascience.com/audio-deep-learning-made-simple-sound-classification-step-by-step-cebc936bbe5
