6138d51dae055fb28420ac2d0d64de0b9937f7dc
Daniel Harper <dghjunior@gmail.com>
2022-04-24 18:50:05 -0400

    Added audio files and created file loop

--- spectrograph.py
+++ spectrograph.py
@@ -1,4 +1,28 @@
-import pase
+from random import sample
 
-pase.pase.start()
+import matplotlib.pyplot as plt
+import os
+from scipy.io import wavfile
 
+
+def createSpectrogram(filename=''):
+    if filename != '':
+        samplingFrequency, signalData = wavfile.read(filename)
+
+        fig = plt.figure()
+        plt.title(filename[39:])
+
+        plt.specgram(signalData, Fs=samplingFrequency)
+        plt.xlabel('Time')
+        plt.ylabel('Frequency')
+
+        plt.show()
+        imgFilename = filename[:len(filename)-3] + 'png'
+        print(imgFilename)
+        fig.savefig(imgFilename)
+
+directory = 'wavFiles'
+for file in os.listdir(directory):
+    f = os.path.join(directory, file)
+    if os.path.isfile(f):
+        createSpectrogram(f)
