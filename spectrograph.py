e8226b16a8d609112129e8d7a93e5ac2d7a9ec67
Daniel Harper <dghjunior@gmail.com>
2022-04-26 09:34:28 -0400

    Atempt at torchaudio spectrogram

--- spectrograph.py
+++ spectrograph.py
@@ -2,6 +2,7 @@ from random import sample
 
 import matplotlib.pyplot as plt
 import os
+from numpy import maximum_sctype
 from scipy.io import wavfile
 
 import torch
@@ -12,6 +13,7 @@ import pandas as pd
 import random
 
 # Source for spectrograph generation https://pythontic.com/visualization/signals/spectrogram
+# Also: https://pytorch.org/tutorials/beginner/audio_preprocessing_tutorial.html
 
 def oldcreateSpectrogram(filename=''):
     if filename != '':
@@ -29,8 +31,53 @@ def oldcreateSpectrogram(filename=''):
         print(imgFilename)
         fig.savefig(imgFilename)
 
-def createSpectrogram():
-    return
+def createSpectrogram(filename=''):
+    max_ms = 7
+    if filename != '':
+        sig, samplerate = torchaudio.load(filename)
+        num_rows, sig_len = sig.shape
+        max_len = samplerate // 1000 * max_ms
+
+        if (sig_len > max_len):
+            sig = sig[:,:max_len]
+
+        elif (sig_len < max_len):
+            pad_begin_len = random.randint(0, max_len - sig_len)
+            pad_end_len = max_len - sig_len - pad_begin_len
+
+            pad_begin = torch.zeros((num_rows, pad_begin_len))
+            pad_end = torch.zeros((num_rows, pad_end_len))
+
+            sig = torch.cat((sig, pad_begin, pad_end), 1)
+
+        waveform = sig.numpy()
+        xlim = None
+        num_channels, num_frames = waveform.shape
+        time_axis = torch.arange(0, num_frames) / samplerate
+
+        figure, axes = plt.subplots(num_channels, 1)
+        if num_channels == 1:
+            axes = [axes]
+        for c in range(num_channels):
+            axes[c].specgram(waveform[c], Fs = samplerate)
+            if num_channels > 1:
+                axes[c].set_ylabel(f'Channel {c+1}')
+            if xlim:
+                axes[c].set_xlim(xlim)
+        figure.suptitle('Spectrogram')
+        plt.show(block = False)
+        '''
+        fig = plt.figure()
+        plt.title(filename[39:])
+        plt.specgram(sig, Fs=samplerate)
+        plt.xlabel('Time')
+        plt.ylabel('Frequency')
+        img_filename = 'imgFiles/' + filename[9:len(filename)-3] + 'png'
+        print(img_filename)
+        fig.savefig(img_filename)
+        '''
+
+        
 
 directory = 'wavFiles'
 for file in os.listdir(directory):
