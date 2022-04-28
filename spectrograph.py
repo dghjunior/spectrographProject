190e9d4afded01f78c73c323d30c9890cf21b7a8
Daniel Harper <dghjunior@gmail.com>
2022-04-28 17:51:27 -0400

    Created spectrograms and added image classifier code

--- spectrograph.py
+++ spectrograph.py
@@ -1,12 +1,16 @@
+from ast import increment_lineno
 from random import sample
-
-import matplotlib.pyplot as plt
 import os
 from numpy import maximum_sctype
 from scipy.io import wavfile
+import matplotlib.pyplot as plt
 
+import numpy as np
 import torch
 import torch.nn as nn
+from torch import optim
+import torch.nn.functional as F
+from torchvision import datasets, transforms, models
 import torchaudio
 from torch.utils.data import DataLoader, Dataset, random_split
 import pandas as pd
@@ -70,6 +74,86 @@ def trim_pad_audio():
             print(str(i) + ', ' + filename)
             torchaudio.save(filename, sig, samplerate)
 
-spectrograph_loop()
+#spectrograph_loop()
 #create_spectrogram('prepped_wav_files/021A-C0897X0004XX-AAZZP0_000407_KDP_2__WHAT-YOU__1675-3025.wav')
 #trim_pad_audio()
+
+data_dir = 'img_files'
+
+def load_split_train_test(datadir, valid_size = .2):
+    train_transforms = transforms.Compose([transforms.Resize(224), transforms.ToTensor(),])
+    test_transforms = transforms.Compose([transforms.Resize(224), transforms.ToTensor(),])
+    train_data = datasets.ImageFolder(datadir, transform=train_transforms)
+    test_data = datasets.ImageFolder(datadir, transform=test_transforms)
+    num_train = len(train_data)
+    indices = list(range(num_train))
+    split = int(np.floor(valid_size * num_train))
+    np.random.shuffle(indices)
+    from torch.utils.data.sampler import SubsetRandomSampler
+    train_idx, test_idx = indices[split:], indices[:split]
+    train_sampler = SubsetRandomSampler(train_idx)
+    test_sampler = SubsetRandomSampler(test_idx)
+    trainloader = torch.utils.data.DataLoader(train_data, sampler = train_sampler, batch_size=64)
+    testloader = torch.utils.data.DataLoader(test_data, sampler = test_sampler, batch_size=64)
+    return trainloader, testloader
+
+trainloader, testloader = load_split_train_test(data_dir, 0.2)
+print(trainloader.dataset.classes)
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model = models.resnet50(pretrained=True)
+print(model)
+
+for param in model.parameters():
+    param.required_grad = False
+model.fc = nn.Sequential(nn.Linear(2048, 512), nn.ReLU(), nn.Dropout(), nn.Linear(512, 10), nn.LogSoftmax(dim=1))
+criterion = nn.NLLLoss()
+optimizer = optim.Adam(model.fc.parameters(), lr=0.003)
+model.to(device)
+
+epochs = 3
+steps = 0
+running_loss = 0
+print_every = 10
+train_losses, test_losses = [], []
+
+for epoch in range(epochs):
+    for inputs, labels in trainloader:
+        steps += 1
+        inputs, labels = inputs.to(device), labels.to(device)
+        optimizer.zero_grad()
+        logps = model.forward(inputs)
+        loss = criterion(logps, labels)
+        loss.backward()
+        optimizer.step()
+        running_loss += loss.item()
+
+        if steps % print_every == 0:
+            test_loss = 0
+            accuracy = 0
+            model.eval()
+            with torch.no_grad():
+                for inputs, labels in testloader:
+                    inputs, labels = inputs.to(device), labels.to(device)
+                    logps = model.forward(inputs)
+                    batch_loss = criterion(logps, labels)
+                    test_loss += batch_loss.item()
+
+                    ps = torch.exp(logps)
+                    top_p, top_class = ps.topk(1, dim=1)
+                    equals = top_class == labels.view(*top_class.shape)
+                    accuracy += torch.mean(equals.type(torch.FloatTensor)).item()
+            train_losses.append(running_loss/len(trainloader))
+            test_losses.append(test_loss/len(testloader))
+            print(f"Epoch {epoch+1}/{epochs}.. "
+                f"Train loss: {running_loss/print_every:.3f}.. "
+                f"Test loss: {test_loss/len(testloader):.3f}.. "
+                f"Test accuracy: {accuracy/len(testloader):.3f}")
+            running_loss = 0
+            model.train()
+torch.save(model, 'aerialmodel.pth')
+
+plt.plot(train_losses, label='Training loss')
+plt.plot(test_losses, label='Validation loss')
+plt.legend(frameon=False)
+plt.show()
